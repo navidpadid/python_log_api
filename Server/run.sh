1153775b411b684cb14fbbe23dc48eef3859b8f0
navidpadid <nmalekgh@uwaterloo.ca>
2024-12-29 16:11:18 -0800

    multiple servers support

--- Server/run.sh
+++ Server/run.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Build the Docker image
 docker build -t python-log-api .
-docker run -d -p 5000:5000 -v $(pwd):/app --name python-log-api-container python-log-api
+
+# Run multiple containers
+for i in {1..3}; do
+    port=$((5000 + i))
+    docker run -d -p $port:5000 -v $(pwd):/app --name python-log-api-container-$i python-log-api
+done
